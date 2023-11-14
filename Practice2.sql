9a86befa4eacdf32ab4502cc6c38bd68f4bf8cc9
Hongnguyenthanthi <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-11-14 18:22:20 +0700

    Update Practice_2.sql

--- Practice2.sql
+++ Practice2.sql
@@ -27,3 +27,36 @@ group by user_id
 having count(post_id)>= 2
 
 -- bai tap 7 
+SELECT card_name,
+max(issued_amount)-min(issued_amount) as difference
+FROM monthly_cards_issued
+group by card_name
+order by difference desc;
+
+-- bai tap 8 
+SELECT manufacturer, count(drug) as drug_count, abs(sum(total_sales - cogs)) as total_loss
+FROM pharmacy_sales
+where total_sales - cogs <=0
+group by manufacturer 
+order by total_loss desc 
+
+-- bai tap 9 
+select * from Cinema
+where id%2 <>0 and not description = 'boring'
+order by rating desc 
+
+-- bai tap 10 
+select teacher_id, count(distinct subject_id) as cnt from Teacher
+group by teacher_id
+
+-- bai tap 11
+select user_id, count(follower_id) as followers_count from Followers
+group by user_id
+order by user_id
+
+-- bai tap 12
+select class
+from Courses
+group by class
+having count(student)>=5
+
