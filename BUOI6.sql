68d486ba388c3291cf84f6870c88939640b689fa
Hongnguyenthanthi <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-11-15 14:13:07 +0700

    Create BUOI6.sql

---
+++ BUOI6.sql
@@ -0,0 +1,21 @@
+--INTERVALS AND TIMESTAMP
+select current_date, current_timestamp, 
+customer_id,
+rental_date,
+return_date,
+extract(day from return_date - rental_date)*24 + extract(hour from return_date - rental_date) || ' '|| 'giờ'
+from rental 
+
+--INTERVALS AND TIMESTAMP solution
+select rental_date, return_date, customer_id
+return_date-rental_date as rental_time
+from rental
+where customer_id = 35
+
+select customer_id,
+avg(return_date-rental_date) as avg_rental_time
+from rental
+group by customer_id
+order by avg(return_date-rental_date) desc 
+
+-- TO_CHAR
