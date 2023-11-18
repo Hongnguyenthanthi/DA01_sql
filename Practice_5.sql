f678354f5c0ce3d7cadbe6239e277a4aa057235d
NguyenThan <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-11-18 13:59:32 +0700

    Update Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -30,3 +30,28 @@ on a.product_id = b.product_id
 where b.product_category in ('Analytics','Containers','Compute') and b.product_name like 'Azure%'
 group by a.customer_id
 having count(a.customer_id)=3
+
+-- bai tap 5
+select mng.reports_to as employee_id, emp.name as name, count(*) as reports_count, round(cast(avg(mng.age) as decimal),0) as average_age
+from Employees as mng
+join Employees as emp
+on mng.reports_to = emp.employee_id
+where mng.reports_to=emp.employee_id
+
+-- bai tap 6
+select a.product_name, sum(b.unit) as unit 
+from products as a 
+join orders as b 
+on a.product_id=b.product_id
+where extract(month from b.order_date)=02 and extract(year from b.order_date)=2020 
+group by a.product_name
+having sum(b.unit)>=100
+
+-- bai tap 7 
+  SELECT a.page_id
+FROM pages as a 
+left join page_likes as b 
+on a.page_id = b.page_id
+where b.page_id is null
+order by a.page_id
+
