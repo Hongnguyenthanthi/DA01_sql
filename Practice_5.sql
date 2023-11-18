fe2c38582fa4cf109ac70d447f39c00bddaf6eea
NguyenThan <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-11-18 15:14:36 +0700

    Update Practice_5.sql

--- Practice_5.sql
+++ Practice_5.sql
@@ -55,3 +55,71 @@ on a.page_id = b.page_id
 where b.page_id is null
 order by a.page_id
 
+------ MID-COURSE
+-- bai tap 1
+select title, replacement_cost
+from film
+order by 
+replacement_cost
+
+-- bai tap 2
+select count(*),
+case when replacement_cost between 9.99 and 19.99 then 'low' 
+when replacement_cost between 20.00 and 24.99 then 'medium'
+when replacement_cost between 25.00 and 29.99 then 'high' end as category 
+from film
+group by category
+
+-- bai tap 3
+select a.title, a.length, c.name
+from film as a
+join film_category as b 
+on a.film_id=b.film_id
+join category as c 
+on b.category_id=c.category_id
+where c.name in ('Sports', 'Drama')
+order by a.length desc 
+
+-- bai tap 4
+select c.name, count(*)
+from film as a
+join film_category as b 
+on a.film_id=b.film_id
+join category as c 
+on b.category_id=c.category_id
+where c.name ='Sports'
+group by c.name
+
+-- bai tap 5
+select a.first_name || ' ' || a.last_name as full_name, count(*)
+from actor as a
+join film_actor as b 
+on a.actor_id=b.actor_id
+group by a.first_name || ' ' || a.last_name
+order by count(*) desc 
+
+-- bai tap 6
+select count(*)
+from address as b
+left join customer as a
+on a.address_id=b.address_id
+where a.address_id is null
+
+-- bai tap 7 
+select a.city, sum(amount)
+from city as a
+join address as b on a.city_id=b.city_id
+join customer as c on b.address_id=c.address_id
+join payment as d on c.customer_id=d.customer_id
+group by a.city
+order by sum(amount) desc 
+
+-- bai tap 8 
+select e.country || ' ' || a.city , sum(amount)
+from country as e
+join city as a on e.country_id=a.country_id
+join address as b on a.city_id=b.city_id
+join customer as c on b.address_id=c.address_id
+join payment as d on c.customer_id=d.customer_id
+group by e.country || ' ' || a.city
+order by sum(amount) 
