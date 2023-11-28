a53581992022022f54b7b5a611f3bad7c26d7c69
NguyenThan <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-11-28 22:51:23 +0700

    Update Project_1.sql

--- Project_1.sql
+++ Project_1.sql
@@ -38,3 +38,24 @@ set contactlastname = upper(left(contactlastname,1)) || lower(right(contactlastn
 update public.sales_dataset_rfm_prj
 set contactfirstname = upper(left(contactfirstname,1)) || lower(right(contactfirstname,length(contactfirstname)-1))
 
+ -- 4. Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
+alter table sales_dataset_rfm_prj
+add column QTR_ID INTEGER
+
+alter table sales_dataset_rfm_prj
+add column MONTH_ID INTEGER
+
+alter table sales_dataset_rfm_prj
+add column YEAR_ID INTEGER
+
+update public.sales_dataset_rfm_prj
+SET MONTH_ID = EXTRACT(MONTH FROM ORDERDATE)
+
+update public.sales_dataset_rfm_prj
+SET YEAR_ID = EXTRACT(YEAR FROM ORDERDATE)
+
+update public.sales_dataset_rfm_prj
+SET QTR_ID = CASE WHEN MONTH_ID BETWEEN 1 AND 3 THEN 1 
+WHEN MONTH_ID BETWEEN 4 AND 6 THEN 2 
+WHEN MONTH_ID BETWEEN 7 AND 9 THEN 3
+WHEN MONTH_ID BETWEEN 10 AND 12 THEN 4 END 
