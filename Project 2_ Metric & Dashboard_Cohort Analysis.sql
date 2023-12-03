7d9012c54ff0f9facae209ae83b76305dd3ce10c
NguyenThan <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-12-03 13:42:54 +0700

    Update Project 2_ Metric & Dashboard_Cohort Analysis.sql

--- Project 2_ Metric & Dashboard_Cohort Analysis.sql
+++ Project 2_ Metric & Dashboard_Cohort Analysis.sql
@@ -1,3 +1,4 @@
+-- join 3 bảng orders, order_items, và products với nhau, group by category, year, month
 with table_1 as 
 (SELECT c.category as product_category, 
 extract(year from a.created_at) as year, 
@@ -13,7 +14,8 @@ join bigquery-public-data.thelook_ecommerce.products as c
 on c.id=b.order_id
 group by 1,2,3
 order by 2,3),
-
+  
+-- lag TPV, lag TPO, tính revenue_growth, order_growth
 table_2 as 
 (select *, 
 lag(TPV) over(partition by product_category order by year, month) as previous_TPV,
@@ -22,18 +24,21 @@ lag(TPO) over(partition by product_category order by year, month) as previous_TP
 (TPO-lag(TPO) over(partition by product_category order by year, month))/lag(TPO) over(partition by product_category order by year, month) as order_growth,
 from table_1),
 
+  -- tìm tháng đầu tiên, năm đầu tiên của từng category
 table_3 as 
 (select *, 
 min(month) over(partition by product_category order by year) as first_month,
 min(year) over(partition by product_category) as first_year
 from table_2),
 
+-- tính cohort_date, index 
 table_4 as (
 select product_category, TPV, TPO, total_profit, 
 (year-first_year)*12+(month-first_month)+1 as index, 
 concat(first_year,'-',first_month) as cohort_date 
 from table_3),
 
+  -- cohort theo TPV
 TPV_cohort as(
 select cohort_date,
 sum(case when index =1 then TPV else 0  end) as m1,
@@ -44,6 +49,7 @@ from table_4
 group by cohort_date
 order by cohort_date)
 
+-- cohort theo TPO
 select cohort_date,
 sum(case when index =1 then TPO else 0  end) as m1,
 sum(case when index =2 then TPO else 0  end) as m2,
