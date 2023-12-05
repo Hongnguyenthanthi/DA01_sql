9140d851fbdc458fb0cdeb9215fdc947658099d7
NguyenThan <150701155+Hongnguyenthanthi@users.noreply.github.com>
2023-12-05 11:11:25 +0700

    Update Project 2.2_ Metric & Dashboard_Cohort Analysis.sql

--- Project 2.2_ Metric & Dashboard_Cohort Analysis.sql
+++ Project 2.2_ Metric & Dashboard_Cohort Analysis.sql
@@ -2,7 +2,7 @@
 with table_1 as 
 (SELECT c.category as product_category, 
 extract(year from a.created_at) as year, 
-extract(month from a.created_at) as month,
+format_date('%Y-%m',a.created_at) as month_year,
   sum(b.sale_price) as TPV,
   count(b.order_id) as TPO,
   sum(c.cost) as total_cost,
@@ -58,6 +58,47 @@ sum(case when index =4 then TPO else 0  end) as m4
 from table_4
 group by cohort_date
 order by cohort_date
+
+cách 2: 
+
+with table1_1  as(select user_id, sale_price, format_date('%Y-%m',date(first_purchase_date)) as cohort_date,
+(extract(year from created_at)-extract(year from first_purchase_date))*12
++ (extract(month from created_at)-extract(month from first_purchase_date))+1 as index
+from  
+(select user_id, sale_price, 
+min(created_at) over(partition by user_id) as first_purchase_date, created_at
+ from bigquery-public-data.thelook_ecommerce.order_items
+ where status = 'Complete')),
+ table2_2 as (
+select cohort_date, index, count(distinct user_id) as cnt, sum(sale_price) as revenue 
+from table1_1
+group by 1,2
+order by cohort_date),
+customer_cohort as (
+select cohort_date, 
+sum(case when index=1 then cnt else 0 end) as t1,
+sum(case when index=2 then cnt else 0 end) as t2,
+sum(case when index=3 then cnt else 0 end) as t3,
+sum(case when index=4 then cnt else 0 end) as t4
+from table2_2
+group by 1
+order by 1),
+retention_cohort as(
+select cohort_date, 
+round(100.00*t1/t1,2)||'%' as t1,
+round(100.00*t2/t1,2)||'%' as t2,
+round(100.00*t3/t1,2)||'%' as t3,
+round(100.00*t4/t1,2)||'%' as t4 
+from customer_cohort)
+select cohort_date,
+(100- round(100.00*t1/t1,2))||'%' as t1,
+(100-round(100.00*t2/t1,2))||'%' as t2,
+(100-round(100.00*t3/t1,2))||'%' as t3,
+(100-round(100.00*t4/t1,2))||'%' as t4
+from customer_cohort
+
+
+
 
 
 
